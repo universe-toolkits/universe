55d9480fca9da065419f70af2aa174cbd75df7e2
Amirhossein Saffari <programer.5892@gmail.com>
2022-10-31 01:35:22 +0330

    add broker methods

--- broker.go
+++ broker.go
@@ -2,17 +2,22 @@ package universe
 
 import (
 	"github.com/universe-toolkits/universe/cachers"
+	err "github.com/universe-toolkits/universe/errors"
 	"github.com/universe-toolkits/universe/loggers"
+	"github.com/universe-toolkits/universe/transporters"
 )
 
 type BrokerOptions struct {
 }
 
 type Broker struct {
-	Transporter string
-	Logger      *loggers.Logger
-	Cacher      *cachers.Cacher
-	Services    map[string]*Service
+	Namespace      string
+	Id             string
+	Transporter    transporters.Transporter
+	Logger         loggers.Logger
+	Cacher         cachers.Cacher
+	Services       map[string]*Service
+	RemoteServices map[string][]*UniverseNode
 }
 
 func (b *Broker) RegisterService(svc *Service) *Broker {
@@ -20,6 +25,20 @@ func (b *Broker) RegisterService(svc *Service) *Broker {
 	return b
 }
 
-func (b *Broker) Call(action string, params map[string]interface{}) map[string]interface{} {
-	return params
+func (b *Broker) Call(action string, params map[string]interface{}) (map[string]interface{}, error) {
+
+	if len(action) == 0 {
+		return nil, err.InvalidActionNameError()
+	}
+
+	am, err := b.Transporter.Call("w", "e", params)
+	return am, err
+}
+
+func (b *Broker) Start() *Broker {
+	return b
+}
+
+func (b *Broker) Stop() *Broker {
+	return b
 }
