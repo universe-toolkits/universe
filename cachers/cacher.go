0dbf3cc70012aced32f24b3f103909e2fbf9da3a
Amirhossein Saffari <programer.5892@gmail.com>
2022-10-31 01:36:18 +0330

    add methods

--- cachers/cacher.go
+++ cachers/cacher.go
@@ -1,4 +1,9 @@
 package cachers
 
 type Cacher interface {
+	Connect() (bool, error)
+	Disconnect() (bool, error)
+	Set(key string, value interface{}) *Cacher
+	Get(key string, object *interface{})
+	Heartbeat() (bool, error)
 }
