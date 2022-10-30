4970200eff5f8c6f81ffbd4dc67a8b8138d28468
Amirhossein Saffari <programer.5892@gmail.com>
2022-10-31 01:37:00 +0330

    add interface methods

--- loggers/logger.go
+++ loggers/logger.go
@@ -1,4 +1,10 @@
 package loggers
 
 type Logger interface {
+	Debug(params ...interface{}) *Logger
+	Info(params ...interface{}) *Logger
+	Success(params ...interface{}) *Logger
+	Warn(params ...interface{}) *Logger
+	Error(params ...interface{}) *Logger
+	Fatal(params ...interface{}) *Logger
 }
